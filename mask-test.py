9ff83412913093ba741087955ea64c5d06e7db5a
Jake Gadaleta <jake.gads@gmail.com>
2021-02-06 21:45:55 -0500

    repaired the youtube dl stuff

--- mask-test.py
+++ mask-test.py
@@ -1,12 +1,15 @@
 import cv2
 import os
+import sys
 from platform import system as operating_system
 
 
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 video_capture = None
 
-if operating_system() != 'Darwin': # for the macs
+is_mac = operating_system() != 'Darwin'
+
+if is_mac and False: # for the macs
     # for camera, It doesn't work on MacOS cause of security features
     video_capture = cv2.VideoCapture(0)
 else:
@@ -20,14 +23,22 @@ else:
     ydl_opts = {}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         url = input('Enter a youtube URL, if not a valid one one will be provided')
+        ydl_opts = {'outtmpl': 'vid'}
         try:
+            
             ydl.download([url])
         except :
-            ydl.download(['https://www.youtube.com/watch?v=DXo2m_XlH4s'])
+            ydl.download(['https://www.youtube.com/watch?v=wv2YhT89LDU'])
     
+    file = ''
+    for i in '3gp,aac,flv,m4a,mp3,mp4,ogg,wav,webm'.split(','):
+        for j in glob(f'*.{i}'):
+            file = f'vidya.{i}'
+            os.rename(j, f'vidya.{i}')
     
-    
-    video_capture = cv2.VideoCapture()
+    print(file)
+
+    video_capture = cv2.VideoCapture(file)
 
 print('begining loop you might have to alt tab out')
 while True:
@@ -36,18 +47,18 @@ while True:
 
     faces = faceCascade.detectMultiScale(
         cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),
-        scaleFactor=2,
-        minNeighbors=4,
-        minSize=(10, 10),
-        maxSize=(30,30)
+        scaleFactor=5,
+        minNeighbors=1,
     )
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
+        
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
+    
 
     if cv2.waitKey(45) & 0xFF == ord('q'):
         break
