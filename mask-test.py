d941a5015fe56e26df0b2d6dc904fa803654c993
Jake Gadaleta <me@jakegads.dev>
2021-02-14 16:58:28 -0500

    took time to clean up code

--- mask-test.py
+++ mask-test.py
@@ -1,36 +1,68 @@
 import cv2
-
+from datetime import datetime
 from video import locate
 
+# how many microseconds for a frame in 30fps
+thirty_fps_as_ms = 19900
+# how many times its allowed to do a switch
+max_skip = 17999
 
-video_capture = locate()    
+def get_curr_time():
+    '''returns the current secode + microsecond as microseconds'''
+    return (datetime.now().second * 1_000_000) + (datetime.now().microsecond)
 
-cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+def main():
+    # fetches the video through youtube-dl
+    video_capture = locate()    
 
-    faces = cascade.detectMultiScale(        
-        cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),
-        scaleFactor=5,
-        minNeighbors=1
-    )
+    # builds a facial casscade from
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
+    # runs for every frame
+    while True:
+        # loops start time
+        start = get_curr_time()
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        # looks for faces in the frame
+        faces = cascade.detectMultiScale(        
+            cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), # converts the frame to B/W 
+            scaleFactor=5, # large possibility
+            minNeighbors=1 # very loose definition on what a face 
+        )
+
+        # Draw a rectangle around the faces
+        # TODO make this place an image instead
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
+            
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+        
+        skip = 0 # the amount of skips
+        for i in range(max_skip):
+            # calculates the difference between the booted time
+            # and the time at this point
+            dif = get_curr_time() - start
+            # check if we are inside the desired range
+            if dif < thirty_fps_as_ms:
+                continue
+            else:
+                break
         
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        # quit button
+        if cv2.waitKey(4) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    video_capture.release()
+    # clear the ram
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
     
-    
-
-
-    if cv2.waitKey(4) & 0xFF == ord('q'):
-        break
-
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
--- video.py
+++ video.py
@@ -2,48 +2,49 @@ from platform import system as operating_system
 import sys
 import os
 import cv2
+import youtube_dl
+from glob import glob
+
+def download():
+    try:
+        # attempts to download for the provided link
+        ydl.download([sys.argv[1]])
+    except :
+        # attempts to download a defaulted url
+        # its an LTT video
+        ydl.download(['https://www.youtube.com/watch?v=ZX7HnNd5PB4'])
 
 def locate():
-    
+    '''finds a video file for analysis''''    
     if operating_system() != 'Darwin' and len(sys.argv) < 1: # for the macs
         # for camera, It doesn't work on MacOS cause of security features
         return cv2.VideoCapture(0)
     else:
-        import youtube_dl
-        from glob import glob
         # Downloads a youtube video
         
         for i in glob('*.webm'):
             os.remove(i)
 
+        # options for the youtube downloader
         ydl_opts = {
-            'outtmpl': 'vid.mp4',
-            'format': 'best[height<=480]',
-            'audio-format': 'mp3'
+            'outtmpl': 'vid.mp4', # saves the file as, defaults to that extent
+            'format': 'best[height<=480]', # dowloads as the best format at 480p (makes things go faster)
+            'audio-format': 'mp3' # caches the audio mp3 because its complaint 
         }
     
+        # uses the options in "safe mode"
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            # attempts to download a file
             try:
-                try:
-                    ydl.download([sys.argv[1]])
-                except :
-                    ydl.download(['https://www.youtube.com/watch?v=ZX7HnNd5PB4'])
+                download()
             except :
+                # sometimes ssl fails this fixes that
                 import ssl
                 ssl._create_default_https_context = ssl._create_unverified_context
-                try:
-                    ydl.download([sys.argv[1]])
-                except :
-                    ydl.download(['https://www.youtube.com/watch?v=ZX7HnNd5PB4'])
+                download()        
         
-        file = ''
-        
-        for i in '3gp,aac,flv,m4a,mp3,mp4,ogg,wav,webm,mkv'.split(','):
-            for j in glob(f'*.{i}'):
-                file = f'{j}'
-
-        print(file)
-        return cv2.VideoCapture(f'{file}')
+        # returns a cv object
+        return cv2.VideoCapture(f'vid.mp4')
 
 if __name__ == '__main__':
     print(sys.argv)
