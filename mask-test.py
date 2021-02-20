129e21029d5e21af2d9873e97d1a3dcb8d12f8ea
Jake Gadaleta <me@jakegads.dev>
2021-02-19 21:30:00 -0500

    places masks

--- mask-test.py
+++ mask-test.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from datetime import datetime
 from video import locate
 
@@ -12,13 +13,47 @@ def get_current_time():
     return (datetime.now().second * 1_000_000) + (datetime.now().microsecond)
 
 
+def overlay_transparent(background, overlay, x, y):
+
+    background_width = background.shape[1]
+    background_height = background.shape[0]
+
+    if x >= background_width or y >= background_height:
+        return background
+
+    h, w = overlay.shape[0], overlay.shape[1]
+
+    if x + w > background_width:
+        w = background_width - x
+        overlay = overlay[:, :w]
+
+    if y + h > background_height:
+        h = background_height - y
+        overlay = overlay[:h]
+
+    if overlay.shape[2] < 4:
+        overlay = np.concatenate(
+            [
+                overlay,
+                np.ones((overlay.shape[0], overlay.shape[1], 1), dtype = overlay.dtype) * 255
+            ],
+            axis = 2,
+        )
+
+    overlay_image = overlay[..., :3]
+    mask = overlay[..., 3:] / 255.0
+
+    background[y:y+h, x:x+w] = (1.0 - mask) * background[y:y+h, x:x+w] + mask * overlay_image
+
+    return background
+
 def main():
     # fetches the video through youtube-dl
     video_capture = locate()    
 
     # builds a facial cascade from
     cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
+    img = cv2.imread('mask.png', cv2.IMREAD_UNCHANGED)
     # runs for every frame
     count = -1
     while True:
@@ -38,7 +73,9 @@ def main():
         # Draw a rectangle around the faces
         # TODO make this place an image instead
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
+            resized = cv2.resize(img, (int(w/1.4), int(h/1.4)), interpolation = cv2.INTER_AREA)
+            frame = overlay_transparent(frame, resized, int(x + w/7), int(y + h/3))
+            # cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
             
 
         # Display the resulting frame
